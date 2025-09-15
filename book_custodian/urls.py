7d1a1ce41b29c90662588d5647f9c3dc1132eda5
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-15 09:49:10 +0300

    learning views

--- book_custodian/urls.py
+++ book_custodian/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
+    path("detail/", views.detail, name="detail"),
     path("", views.index, name="index"),
 ]
--- book_custodian/views.py
+++ book_custodian/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Book Custodian Successfully Integrated")
+
+custodians_data = {
+    "name": "Maria",
+    "age": 28,
+    "department": "Theology",
+    "salary": "50000"
+}
+
+def detail(request):
+    return HttpResponse(f"<h2>{custodians_data.get("name", {})}, a {custodians_data.get("age", {})} year old lady works at the department of {custodians_data.get("department", {})} for a salary of ${custodians_data.get("salary", {})} per month.</h2>")
