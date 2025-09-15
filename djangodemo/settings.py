c55a2621a115e7a245c698281021400ab14bf379
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-15 11:23:34 +0300

    database setup

--- djangodemo/settings.py
+++ djangodemo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangodemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learndjango',
+        'USER': 'root',
+        'PASSWORD': "learn-django-pw", # never hardcode your credentials
+        'HOST': '127.0.0.1',
+        'PORT': 60,
     }
 }
 
