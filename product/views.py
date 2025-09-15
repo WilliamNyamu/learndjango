a402b99899e4ea50fa31f58ba2505cfd7347918c
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-15 22:15:30 +0300

    views configure

--- product/views.py
+++ product/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Product
+
 def index(request):
     return HttpResponse("<h1>Christ is my firm foundation</h1>")
