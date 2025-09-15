8c1788b8e08f7ef59d1551690df3d847ca68647a
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-15 15:13:44 +0300

    Made book_custodian migrations and added books app

--- book_custodian/models.py
+++ book_custodian/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Custodians(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    department = models.CharField(max_length=200)
+    salary = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, a {self.age} years old works in the {self.department} department for {self.salary} per month"
+    
+
+    
