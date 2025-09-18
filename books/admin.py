dff25be35af2edcab8dcaf15d566a4811c33dec6
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-18 05:53:33 +0300

    Admin customiation, using @admin decorator and class ModelAdmin

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Book, Author
 
+# admin.site.empty_value_display = "Unknown" -> Customizing empty value display for all models
+# To change how a model is displayed in the admin interface, you define a ModelClass (which describes the layout) and register it with the model
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
+
+#Method 1
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'birth_date_view', 'biography']
+
+    # Customizing empty_value display for a particular model
+    @admin.display(empty_value="Yet")
+    def birth_date_view(self, obj):
+        return obj.birth_date
+    
+admin.site.register(Author, AuthorAdmin)
+
+# Method 2
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'genre','pages']
+    list_filter = ['author', 'genre']
