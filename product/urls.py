dd34fd5968334dd45e62818286f48ff876436db3
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-20 14:42:47 +0300

    Learnt class based views

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("list_products/", views.list_products, name="list_products"),
+    path("", views.ListProductView.as_view(), name="product_list")
 ]
--- product/views.py
+++ product/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 # Create your views here.
 
 from .models import Product
 
-def index(request):
-    return HttpResponse("<h1>Christ is my firm foundation</h1>")
+def list_products(request):
+    products = Product.objects.all()
+    return render(request, "product/product_list.html", {'product': products})
+
+
+class ListProductView(ListView):
+    model = Product
+    template_name = "product/product_list.html"
+    context_object_name = "product"
